0d082feb4300391067bf245218cf13291f6537a5
BRCNbysl <100028109+BRCNbysl@users.noreply.github.com>
2023-07-10 14:49:36 +0300

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -207,7 +207,7 @@ with siteHeader:
     
     df2 = pd.DataFrame({'OPERASYON SAYISI' : int(OPERASYON_SAYISI), 'KAMLI DELİK' : int(KAMLI_DELIK),
                      'ZORLUK KATSAYISI' :int(ZORLUK_KATSAYISI), 'TOPLAM AĞIRLIK' : float(kalip_agirligi),'KALIP TİPİ_0' :int(kalip_postali) ,
-                     'KALIP TİPİ_1' : int(kalip_prograsif), 'KALIP TİPİ_2' : int(kalip_tandem), 'KALIP TİPİ_3' : int(kalip_transfer)})
+                     'KALIP TİPİ_1' : int(kalip_prograsif), 'KALIP TİPİ_2' : int(kalip_tandem), 'KALIP TİPİ_3' : int(kalip_transfer)}, index=[0])
 
   
   
