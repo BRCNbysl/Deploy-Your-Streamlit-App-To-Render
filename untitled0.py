82711144b88bdc8f0b24a1b4c4b3d48cc7e9524f
BRCNbysl <100028109+BRCNbysl@users.noreply.github.com>
2023-07-10 14:14:17 +0300

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -205,11 +205,11 @@ with siteHeader:
     
     
     
-    df2 = pd.Dataframe({'OPERASYON SAYISI' : int(OPERASYON_SAYISI), 'KAMLI DELİK' : int(KAMLI_DELIK),
+    df2 = df.append({'OPERASYON SAYISI' : int(OPERASYON_SAYISI), 'KAMLI DELİK' : int(KAMLI_DELIK),
                      'ZORLUK KATSAYISI' :int(ZORLUK_KATSAYISI), 'TOPLAM AĞIRLIK' : float(kalip_agirligi),'KALIP TİPİ_0' :int(kalip_postali) ,
-                     'KALIP TİPİ_1' : int(kalip_prograsif), 'KALIP TİPİ_2' : int(kalip_tandem), 'KALIP TİPİ_3' : int(kalip_transfer)})
+                     'KALIP TİPİ_1' : int(kalip_prograsif), 'KALIP TİPİ_2' : int(kalip_tandem), 'KALIP TİPİ_3' : int(kalip_transfer)}, ignore_indeks = True)
   
-    df3=pd.concat([df2, df], ignore_indeks =   True)
+    
     
 
    
@@ -227,7 +227,7 @@ with siteHeader:
     
 
     if submitted:
-        result = ValuePredictor(df3)*float(BIRIM_FIYAT)
+        result = ValuePredictor(df2)*float(BIRIM_FIYAT)
         st.markdown('<p class="navbar"> Öngörülen Kalıp Maliyeti (€) </p>', unsafe_allow_html=True)
         st.write(result[0])
         print(result)
